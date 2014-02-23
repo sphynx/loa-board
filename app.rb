949a39c038ef0a69b8a0a21664c152fc1315d43f
Ivan Veselov <veselov@gmail.com>
2014-02-23 13:16:50 +0200

    Added variant recognition by parsing HTML with the game.
    This won't be necessary when Richard introduce a variant field in LOA PGN.

--- app.rb
+++ app.rb
@@ -1,24 +1,48 @@
 require 'sinatra'
 require 'open-uri'
 
-LG_URL = 'http://www.littlegolem.net/jsp/game/png.jsp'
+LG_PGN_URL = 'http://www.littlegolem.net/jsp/game/png.jsp'
+LG_GAME_URL = 'http://www.littlegolem.net/jsp/game/game.jsp'
+
+VARIANT_8x8_RE = /Lines of Action-Size 8x8/
+VARIANT_BLACKHOLE_RE = /Lines of Action-Black Hole/
+VARIANT_SCRAMBLE_RE = /Lines of Action-Scramble/
+VARIANT_QUICK_RE = /Lines of Action-Quick/
+
+def detect_loa_variant(url)
+  open(url) do |f|
+    f.each_line do |line|
+      case line
+      when VARIANT_8x8_RE
+        return '8x8'
+      when VARIANT_BLACKHOLE_RE
+        return 'BlackHole'
+      when VARIANT_SCRAMBLE_RE
+        return 'Scramble'
+      when VARIANT_QUICK_RE
+        return 'Quick'
+      end
+    end
+    return 'Unknown'
+  end
+end
 
 get '/' do
   $pgn = ''
+  $variant = '8x8'
   erb :index
 end
 
 get '/lg/:id' do
   id = params[:id]
   $game_id = id
-  url = "#{LG_URL}?gid=#{id}"
 
-  open(url) do |f|
-    $pgn = ""
-    f.each_line { |line| $pgn += line }
-  end
+  pgn_url = "#{LG_PGN_URL}?gid=#{id}"
+  $pgn = open(pgn_url).read().gsub(/\r|\n/, "")
 
-  $pgn.gsub!(/\r|\n/, "")
+  game_url = "#{LG_GAME_URL}?gid=#{id}"
+  $variant = detect_loa_variant(game_url)
 
   erb :index
 end
+
